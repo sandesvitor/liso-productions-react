e6fd7f2020795303a92a56164c27a56f76186c44
Vitor Sandes <sandesvitor@gmail.com>
2020-09-09 12:37:23 -0300

    Fxed SmothScroller css and transforme it in a more generic component

--- src/components/tools/SmothScroller.jsx
+++ src/components/tools/SmothScroller.jsx
@@ -4,18 +4,19 @@ import './SmothScroller.css'
 
 
 export default props =>
-    <SmothLink
-        activeClass="active"
-        to={props.goToId}
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={props.scrollDuration}>
+    <div className="smoth-scroller-container">
+        <SmothLink
+            activeClass="active"
+            to={props.goToId}
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={props.scrollDuration}>
 
-        <div className="smoth-scroller-container">
+
             <img
                 src={require('../../assets/misc/smothScroll.png')}
                 alt="Smoth Scroller icon" />
-        </div>
 
-    </SmothLink>
+        </SmothLink>
+    </div>
